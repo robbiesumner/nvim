190a0c54108bc79eaaf8f05d36a24848420e53f8
robbie <robbiecsumner@gmail.com>
2024-01-07 15:51:57 +0100

    format

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require 'nvim-treesitter.configs'.setup {
+require "nvim-treesitter.configs".setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = { "javascript", "typescript", "c", "lua", "rust" },
 
@@ -6,7 +6,7 @@ require 'nvim-treesitter.configs'.setup {
     sync_install = false,
 
     -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
     auto_install = true,
 
     highlight = {
--- lua/robbie/lazy.lua
+++ lua/robbie/lazy.lua
@@ -1,9 +1,10 @@
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+    -- bootstrap lazy.nvim
+    -- stylua: ignore
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+        lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -14,32 +15,33 @@ require("lazy").setup({
         "catppuccin/nvim",
         name = "catppuccin",
         config = function()
-            vim.cmd('colorscheme catppuccin')
+            vim.cmd("colorscheme catppuccin")
         end
     },
 
     {
-        'nvim-telescope/telescope.nvim', version = '0.1.5',
-        -- or                            , branch = '0.1.x',
-        dependencies = { { 'nvim-lua/plenary.nvim' } }
+        "nvim-telescope/telescope.nvim",
+        version = "0.1.5",
+        -- or                            , branch = "0.1.x",
+        dependencies = { { "nvim-lua/plenary.nvim" } }
     },
 
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
         dependencies = {
             --- Uncomment these if you want to manage the language servers from neovim
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },
+            { "neovim/nvim-lspconfig" },
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "L3MON4D3/LuaSnip" },
         }
     },
 
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 })
