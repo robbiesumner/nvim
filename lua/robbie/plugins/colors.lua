da05fb220c0905b7a61c17894ac98d21641bb7c3
Robbie Sumner <robbiecsumner@gmail.com>
2024-01-15 21:46:25 +0100

    transparent background

--- lua/robbie/plugins/colors.lua
+++ lua/robbie/plugins/colors.lua
@@ -3,6 +3,9 @@ return {
         "folke/tokyonight.nvim",
         config = function()
             vim.cmd("colorscheme tokyonight-night")
+
+            -- set transparent background
+            vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
         end
     }
 }
