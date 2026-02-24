6c47795e6d005e4f8d08d5ff37604e081a632fc0
Robbie Sumner <robbiecsumner@gmail.com>
2026-02-24 15:31:12 +0100

    feat: :art: create home manager module to use

--- flake.nix
+++ flake.nix
@@ -7,99 +7,50 @@
   outputs = {
     self,
     nixpkgs,
+    ...
   }: let
-    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    mkNeovim = import ./lib/myNeovim.nix;
   in {
     packages = forAllSystems (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-
-        binPath = pkgs.lib.makeBinPath (with pkgs; [
-          ripgrep
-          tree-sitter
-          # language servers
-          lua-language-server
-          nixd
-          pyright
-          yaml-language-server
-          vscode-css-languageserver
-          # formatters
-          stylua
-          alejandra
-          ruff
-          # molten
-          imagemagick
-          python3Packages.jupytext
-          quarto
-        ]);
-
-        pythonEnv = pkgs.python3.withPackages (ps:
-          with ps; [
-            pynvim
-            jupyter-client
-            nbformat
-            pillow
-            pnglatex
-            requests
-          ]);
-
-        vimPlugins = with pkgs.vimPlugins; [
-          catppuccin-nvim
-          nvim-lspconfig
-          lazydev-nvim
-          nvim-treesitter.withAllGrammars
-          lualine-nvim
-          fzf-lua
-          yuck-vim
-          gitsigns-nvim
-          blink-cmp
-          conform-nvim
-          mini-icons
-          which-key-nvim
-          # data science
-          molten-nvim
-          image-nvim
-          jupytext-nvim
-          quarto-nvim
-          otter-nvim
-        ];
-
-        neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
-          withRuby = false;
-          plugins = vimPlugins;
-          customLuaRC = ''
-            -- setup python
-            vim.g.python3_host_prog = "${pythonEnv}/bin/python3"
-
-
-            vim.opt.runtimepath:prepend("${self}/")
-            vim.opt.runtimepath:append("${self}/after")
-            dofile("${self}/init.lua")
-          '';
+      system: {
+        default = mkNeovim {
+          inherit (nixpkgs.legacyPackages.${system}) pkgs lib;
+          inherit self;
+          supportJupyter = true;
         };
-
-        wrappedNeovim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (neovimConfig
-          // {
-            wrapperArgs =
-              neovimConfig.wrapperArgs
-              ++ [
-                "--prefix"
-                "PATH"
-                ":"
-                "${binPath}"
-              ];
-          });
-      in {
-        default = wrappedNeovim;
+        noJupyter = mkNeovim {
+          inherit (nixpkgs.legacyPackages.${system}) pkgs lib;
+          inherit self;
+          supportJupyter = false;
+        };
       }
     );
-
-    apps = forAllSystems (system: {
-      default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/nvim";
+    homeManagerModules.default = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: let
+      cfg = config.programs.custom-neovim;
+    in {
+      options.programs.custom-neovim = {
+        enable = lib.mkEnableOption "My custom neovim";
+        supportJupyter = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+        };
       };
-    });
+      config = lib.mkIf cfg.enable {
+        home.packages = [
+          (mkNeovim {
+            inherit pkgs lib self;
+            inherit (cfg) supportJupyter;
+          })
+        ];
+        home.sessionVariables.EDITOR = "nvim";
+      };
+    };
   };
 }
---
+++ lib/myNeovim.nix
@@ -0,0 +1,89 @@
+{
+  pkgs,
+  lib,
+  self,
+  supportJupyter ? false,
+}: let
+  binPath = lib.makeBinPath (with pkgs;
+    [
+      ripgrep
+      tree-sitter
+      lua-language-server
+      nixd
+      pyright
+      yaml-language-server
+      vscode-css-languageserver
+      stylua
+      alejandra
+      ruff
+      imagemagick
+    ]
+    ++ lib.optionals supportJupyter [
+      python3Packages.jupytext
+      quarto
+    ]);
+
+  pythonEnv = pkgs.python3.withPackages (ps:
+    with ps;
+      [
+        pynvim
+      ]
+      ++ lib.optionals supportJupyter [
+        jupyter-client
+        nbformat
+        pillow
+        pnglatex
+        requests
+      ]);
+
+  vimPlugins = with pkgs.vimPlugins;
+    [
+      catppuccin-nvim
+      nvim-lspconfig
+      lazydev-nvim
+      nvim-treesitter.withAllGrammars
+      lualine-nvim
+      fzf-lua
+      yuck-vim
+      gitsigns-nvim
+      blink-cmp
+      conform-nvim
+      mini-icons
+      which-key-nvim
+      image-nvim
+    ]
+    ++ lib.optionals supportJupyter [
+      molten-nvim
+      jupytext-nvim
+      quarto-nvim
+      otter-nvim
+    ];
+
+  neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
+    withRuby = false;
+    plugins = vimPlugins;
+    customLuaRC = ''
+      vim.g.python3_host_prog = "${pythonEnv}/bin/python3"
+      vim.opt.runtimepath:prepend("${self}/")
+      vim.opt.runtimepath:append("${self}/after")
+      dofile("${self}/init.lua")
+
+      vim.g.jupyter = ${
+        if supportJupyter
+        then "true"
+        else "false"
+      }
+    '';
+  };
+in
+  pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (neovimConfig
+    // {
+      wrapperArgs =
+        neovimConfig.wrapperArgs
+        ++ [
+          "--suffix"
+          "PATH"
+          ":"
+          "${binPath}"
+        ];
+    })
