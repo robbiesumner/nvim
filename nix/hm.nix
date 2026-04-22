a0ba29c95cb323ef862e326cafc3eeefe668e89d
Robbie Sumner <robbiecsumner@gmail.com>
2026-04-22 02:42:38 +0200

    chore: update to initLua

--- nix/hm.nix
+++ nix/hm.nix
@@ -6,7 +6,7 @@ in {
     enable = true;
     extraPackages = dependencies;
     plugins = plugins;
-    extraLuaConfig = ''
+    initLua = ''
       vim.opt.rtp:prepend("${src}")
       vim.opt.rtp:append("${src}/after")
       vim.g.soundfont_path = "${pkgs.soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2"
